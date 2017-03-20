83391cb7685ed05e8a9824a87d8a20539f1146b2
Chuck B <Chuck B>
2017-03-19 21:06:18 -0400

    Fixed to allow spaces in the textbox for locales that require a space after the date separator.
    Signed-off-by: Chuck B <chuck@chuckbeasley.com>

--- src/my-date-picker/directives/my-date-picker.numeric.directive.ts
+++ src/my-date-picker/directives/my-date-picker.numeric.directive.ts
@@ -4,9 +4,9 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
     selector: "[myinputnumeric]"
 })
 export class NumericDateDirective {
-    private regexStr = "^[0-9/.-]*$";
+    private regexStr = "^[0-9/.- ]*$";
     private allowedKeys: Array<string> = ["Backspace", "Tab", "End", "Home", "ArrowLeft", "ArrowRight"];
-    constructor(private element: ElementRef) {       
+    constructor(private element: ElementRef) {  
     }
     @HostListener("keydown", ["$event"]) onKeyDown(event: KeyboardEvent) {
         if (this.allowedKeys.indexOf(event.key) !== -1) {
